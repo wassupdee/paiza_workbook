30ef220c8327f7f3b38cb4027a8b73d34fa921a5
wassupdee <daichi.hirohata.japan@gmail.com>
2024-08-12 14:33:20 +0900

    出力問題

--- D_rank.rb
+++ D_rank.rb
@@ -11,3 +11,17 @@ puts 13
 
 # 「【改行あり出力】1,000 行の出力 Ruby編」
 (1..1000).each { |n| puts n  }
+
+# 2 つの数値を出力 (paizaランク D 相当)
+array = [1, 1]
+puts array.join(' ')
+
+# 3 つの数値を出力
+array = [8, 1, 3]
+puts array.join(' ')
+
+10 個の数値を出力 (末尾に半角スペース有) Ruby編
+(1..10).each do|n|
+  print n
+  print ' '
+end
