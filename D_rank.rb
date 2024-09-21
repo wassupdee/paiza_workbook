2ad9534d720081620a6195e6f5521575cf9184d1
Daichi <156046661+wassupdee@users.noreply.github.com>
2024-09-21 13:54:16 +0900

    Update D_rank.rb

--- D_rank.rb
+++ D_rank.rb
@@ -28,3 +28,29 @@ end
 
 #1000個の数値を出力
 puts (1..1000).to_a.join(' ')
+
+# 入力された 10 個の文字列を出力 (paizaランク D 相当)
+a = []
+10.times { a << gets.chomp }
+puts a.join(" ")　# 配列にjoinメソッドを使うと、各要素の間に文字列（空欄）を入れながら、最後は改行される
+
+# 【文字列の出力】入力された 10 個の文字列を出力 (paizaランク D 相当)
+a = gets.split # 引数を渡さないと自動的に、split(nil)となる。splitを使うと、配列になる　["a", "a", "a", "a", "a", "a", "a", "a", "a", "a"]
+puts a # putsは配列の各要素を出力するのにも使える
+
+# 1 行または 2 行の出力 (paizaランク D 相当)
+input_line = gets.chomp.to_i # 入力は文字列になるので、数値に変換することが必要！
+if input_line == 1
+    puts 1
+elsif input_line == 2
+    puts 1, 2
+end
+
+# 数行の出力 (paizaランク D 相当)
+last = gets.chomp.to_i
+(1..last).each {|n| puts n }
+
+# カンマ区切りで 2 つ出力 (paizaランク D 相当)
+input = gets
+a = input.split
+puts a.join(",")
