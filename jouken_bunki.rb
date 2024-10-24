d35cfba1fe6e2e680cdf7835bf59145089b8ac14
Daichi <156046661+wassupdee@users.noreply.github.com>
2024-10-24 09:40:07 +0900

    Create jouken_bunki.rb

---
+++ jouken_bunki.rb
@@ -0,0 +1,18 @@
+# 0 が含まれていないか判定
+n = gets.to_i
+
+a = []
+n.times do
+    a << gets.to_i
+end
+
+b = a.select { |k| k == 0 }
+if b.empty?
+    puts "YES"
+else puts "NO"
+end
+
+## 別回答
+n = gets.to_i
+a = n.times.map { gets.to_i }　# mapは基本的に配列から要素を取り出して処理をし、再度配列をつくる。しかし、このようにいきなり処理から始め、それを要素とする配列をつくることも可能
+puts a.include?(0) ? "NO" : "YES
