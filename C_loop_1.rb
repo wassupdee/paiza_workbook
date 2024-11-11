9fca44dfac97b0249ab143c1e08bfbcee785e31d
Daichi <156046661+wassupdee@users.noreply.github.com>
2024-11-11 10:59:57 +0900

    Update C_loop_1.rb

--- C_loop_1.rb
+++ C_loop_1.rb
@@ -2,3 +2,15 @@
 ('a'..'z').each do |i|
     puts i
 end
+
+
+# 数列同士の引き算
+n = gets.to_i
+a = gets.split.map(&:to_i)
+b = gets.split.map(&:to_i)
+c = []
+n.times do |i| # timesメソッドは、0~nまでの整数を1つずつ使用する
+  c << a[i] - b[i]
+end
+
+puts c
