0f7cd52f929707011554fccfec42529804c9a468
Daichi <156046661+wassupdee@users.noreply.github.com>
2024-11-13 08:44:02 +0900

    Update C_loop_2.rb

--- C_loop_2.rb
+++ C_loop_2.rb
@@ -14,3 +14,12 @@ end
 a = gets.to_i
 b = gets.split.map(&:to_i)
 b.each do { |n| puts i.even? ? "even" : "odd" }
+
+
+# 約数の列挙
+n = gets.to_i
+(1..n).each do |i|
+    if n % i == 0
+        puts i
+    end
+end
