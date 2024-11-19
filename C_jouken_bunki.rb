e33316c46854a400107e7a09dff94361a987ddd0
Daichi <156046661+wassupdee@users.noreply.github.com>
2024-11-19 19:11:22 +0900

    Update and rename jouken_bunki.rb to C_jouken_bunki.rb

--- C_jouken_bunki.rb
+++ C_jouken_bunki.rb
@@ -12,7 +12,17 @@ if b.empty?
 else puts "NO"
 end
 
-## 別回答
+# 別回答
 n = gets.to_i
 a = n.times.map { gets.to_i }　# mapは基本的に配列から要素を取り出して処理をし、再度配列をつくる。しかし、このようにいきなり処理から始め、それを要素とする配列をつくることも可能
-puts a.include?(0) ? "NO" : "YES
+puts a.include?(0) ? "NO" : "YES"
+
+# AND+OR
+x, y, z = gets.split.map(&:to_i)
+
+if z >= 10
+    puts "YES"
+elsif x >= 10 && y >= 10
+    puts "YES"
+else puts "NO"
+end
