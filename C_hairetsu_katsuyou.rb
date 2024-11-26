2812d3d2b44f4d932656eccd001a60c24aaaf946
Daichi <156046661+wassupdee@users.noreply.github.com>
2024-11-26 09:07:09 +0900

    Update C_hairetsu_katsuyou.rb

--- C_hairetsu_katsuyou.rb
+++ C_hairetsu_katsuyou.rb
@@ -6,3 +6,17 @@ a.times do
     c << gets.to_i + b
 end
 puts c # 配列を出力すれば、すべての要素が改行で出力される
+
+# 【配列への副作用を伴う操作】条件を満たす要素のみの配列作成
+n, k = gets.split.map(&:to_i)
+a = []
+
+n.times do
+    a << gets.to_i
+end
+
+b = a.select do |j|
+  j >= k
+end
+
+puts b
